e191b971439fddaf61209fe513492567a3d762fb
AQUIN0S <aquinos@acyonix.com>
2017-06-18 15:23:09 +1200

    Removed the methods to display the order summary and instead created a method to send the order summary via an intent to an email app.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
+
 import butterknife.ButterKnife;
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -15,13 +20,16 @@ import butterknife.OnClick;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private int quantityValue = 0;
+    @BindView(R.id.quantity_text_view)
+    TextView quantityTextView;
+    @BindView(R.id.whipped_cream_check)
+    CheckBox whippedCreamCheck;
+    @BindView(R.id.chocolate_check)
+    CheckBox chocolateCheck;
+    @BindView(R.id.name_edit_text_view)
+    EditText nameEditTextView;
 
-    @BindView(R.id.quantity_text_view) TextView quantityTextView;
-    @BindView(R.id.order_summary_text_view) TextView orderSummaryTextView;
-    @BindView(R.id.whipped_cream_check) CheckBox whippedCreamCheck;
-    @BindView(R.id.chocolate_check) CheckBox chocolateCheck;
-    @BindView(R.id.name_edit_text_view) EditText nameEditTextView;
+    private int quantityValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        displayQuantity();
 
     }
 
@@ -44,8 +53,9 @@ public class MainActivity extends AppCompatActivity {
         boolean whippedCream = whippedCreamCheck.isChecked();
         boolean chocolate = chocolateCheck.isChecked();
         String name = nameEditTextView.getText().toString();
-        String message = createOrderSummary(name, price, whippedCream, chocolate);
-        displayMessage(message);
+        String message = createMessage(name, price, whippedCream, chocolate);
+
+        sendEmail(message);
 
     }
 
@@ -57,20 +67,27 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.increment_button)
     public void increment() {
 
-        this.quantityValue += 1;
-        displayQuantity(this.quantityValue);
+        if (quantityValue >= 100) {
+            Toast.makeText(this, "Too many cups of coffee!", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        quantityValue += 1;
+        displayQuantity();
 
     }
 
     @OnClick(R.id.decrement_button)
     public void decrement() {
 
-        if (this.quantityValue <= 0) {
-            this.quantityValue = 0;
-        } else {
-            this.quantityValue -= 1;
+        if (quantityValue <= 0) {
+            Toast.makeText(this, "Cannot have less than 0 cups of coffee!", Toast.LENGTH_SHORT).show();
+            return;
         }
-        displayQuantity(this.quantityValue);
+
+        quantityValue -= 1;
+        displayQuantity();
 
     }
 
@@ -81,7 +98,16 @@ public class MainActivity extends AppCompatActivity {
 
     private double calculatePrice() {
 
-        return this.quantityValue * 2.70;
+        double basePrice = 2.70;
+
+        if (whippedCreamCheck.isChecked()) {
+            basePrice += 1.00;
+        }
+        if (chocolateCheck.isChecked()) {
+            basePrice += 2.00;
+        }
+
+        return basePrice * quantityValue;
 
     }
 
@@ -90,29 +116,48 @@ public class MainActivity extends AppCompatActivity {
      * Methods to display quantity and to create and display the order summary
      */
 
-    private void displayQuantity(int quantity) {
+    private void displayQuantity() {
 
-        String value = Integer.toString(quantity);
-        this.quantityTextView.setText(value);
+        String value = Integer.toString(quantityValue);
+
+        quantityTextView.setText(value);
 
     }
 
-    private String createOrderSummary(String name, double price, boolean whippedCream, boolean chocolate) {
-
+    private String createMessage(String name, double price, boolean whippedCream, boolean chocolate) {
 
         String message = "Name: " + name;
-        message += "\nAdd whipped cream? " + whippedCream;
-        message += "\nAdd chocolate? " + chocolate;
-        message += "\nQuantity: " + this.quantityValue;
+        message += "\nAdd whipped cream? ";
+        if (whippedCream) {
+            message += "Yes";
+        } else {
+            message += "No";
+        }
+        message += "\nAdd chocolate? ";
+        if (chocolate) {
+            message += "Yes";
+        } else {
+            message += "No";
+        }
+        message += "\nQuantity: " + quantityValue;
         message += "\nTotal: " + NumberFormat.getCurrencyInstance().format(price);
         message += "\nThank you!";
+
         return message;
 
     }
 
-    private void displayMessage(String message) {
+    private void sendEmail(String message) {
 
-        this.orderSummaryTextView.setText(message);
+        String subject = "Coffee Order";
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
     }
 
